12ec97cbe90bef0c452f76bf1a83149ab99af597
ShankarAnush <anushalva3@gmail.com>
2024-08-02 12:26:56 +0200

    [QA] Resolving resource preexisting error

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -9,7 +9,7 @@ def test_integration():
     client = boto3.client(
         'lambda',
         region_name=os.getenv('AWS_REGION'),
-        aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
     )
     try:
