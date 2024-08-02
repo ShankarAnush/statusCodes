05670b97b1e5e9be0e2d3d29a61a825c3e1d9cef
ShankarAnush <anushalva3@gmail.com>
2024-08-02 11:53:43 +0200

    [CI] Access credentials as GitHub secrets

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -1,16 +1,24 @@
+import os
+import json
 import pytest
 import boto3
 from botocore.exceptions import ClientError
 
 
 def test_integration():
-    client = boto3.client('lambda', region_name='us-west-2')
+    client = boto3.client(
+        'lambda',
+        region_name=os.getenv('AWS_REGION'),
+        aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+    )
     try:
         response = client.invoke(
             FunctionName='statusCodeHandler',
             InvocationType='RequestResponse',
         )
-        payload = response['Payload'].read()
+        payload = json.loads(response['Payload'].read())
+        expected_status_codes = {200, 300, 400, 500, 501, 503, 507}
         print(payload)
     except ClientError as e:
         pytest.fail(f"Lambda invocation failed: {e}")
