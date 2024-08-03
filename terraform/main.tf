a8864e86cecc71180c6498b63871d52c941aad09
ShankarAnush <anushalva3@gmail.com>
2024-08-03 10:34:03 +0200

    [QA] Integration Tests

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,26 @@ resource "aws_iam_role" "lambda_role" {
   ]
 }
 
+resource "aws_iam_role_policy" "lambda_access_policy" {
+  name   = "LambdaAccessPolicy"
+  role   = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction",
+          "lambda:GetFunctionConfiguration",
+          "lambda:ListFunctions"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../src"
@@ -53,6 +73,12 @@ resource "aws_api_gateway_rest_api" "status_codes_api" {
   description = "API to return random HTTP status codes"
 }
 
+resource "aws_api_gateway_api_key" "api_key" {
+  name = "APIKey"
+  description = "API key for the Status Codes API"
+  enabled = true
+}
+
 resource "aws_api_gateway_resource" "statuscode_resource" {
   rest_api_id = aws_api_gateway_rest_api.status_codes_api.id
   parent_id   = aws_api_gateway_rest_api.status_codes_api.root_resource_id
@@ -83,15 +109,7 @@ resource "aws_lambda_permission" "apigw_invoke" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.status_codes_function.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.status_codes_api.id}/*"
-}
-
-resource "aws_api_gateway_deployment" "status_codes_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.status_codes_api.id
-  stage_name  = "prod"
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration
-  ]
+  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.status_codes_api.id}/*/*/*"
 }
 
 resource "aws_api_gateway_usage_plan" "usage_plan" {
@@ -106,18 +124,19 @@ resource "aws_api_gateway_usage_plan" "usage_plan" {
   }
 }
 
-resource "aws_api_gateway_api_key" "api_key" {
-  name = "APIKey"
-  description = "API key for the Status Codes API"
-  enabled = true
-}
-
 resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
   key_id = aws_api_gateway_api_key.api_key.id
   key_type = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.usage_plan.id
 }
 
+resource "aws_api_gateway_deployment" "status_codes_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.status_codes_api.id
+  stage_name  = "prod"
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration
+  ]
+}
 
 resource "aws_cloudwatch_metric_alarm" "_5xx_alarm" {
   alarm_name          = "5xxErrorAlarm"
