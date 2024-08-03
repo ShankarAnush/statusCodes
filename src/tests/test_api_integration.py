88dbfacdd9dbf724a3eb4f66e35143d00be39dad
ShankarAnush <anushalva3@gmail.com>
2024-08-03 11:29:50 +0200

    [QA] Integration Tests

--- src/tests/test_api_integration.py
+++ src/tests/test_api_integration.py
@@ -7,8 +7,5 @@ def test_api_endpoint():
     """test the integration of api endpoint"""
     headers = {"x-api-key":os.getenv('API_KEY')}
     response = requests.get(url=os.getenv('API_URL'), headers=headers, timeout=5)
-
-    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
-
     status_code = response.json().get('statusCode')
     assert status_code in [200, 300, 400, 500, 501, 503, 507], f"Unexpected code {status_code}"
